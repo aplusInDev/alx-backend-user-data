c7903445ac193b2497eb3402f0faa20714dc2ce1
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-02-09 03:01:54 +0100

    all done

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" This module provides functions to hash and validate passwords."""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password, which is a byte string."""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Returns a boolean indicating whether the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
